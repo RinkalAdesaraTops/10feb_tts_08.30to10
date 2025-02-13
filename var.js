cfbd1c3e6a364c83860d6a42490f24c267181f73
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2025-02-13 09:49:39 +0530

    operators

---
+++ var.js
@@ -0,0 +1,26 @@
+// 1)Nothing
+a=25
+b=35
+c=a+b
+document.write('<br> Addition is '+(a+b))
+console.log("minus is ",a-b);
+// 2)Var keyword -- redeclare & reassign possible
+var p = 200
+var q=10
+document.write('<br> Multiplication is '+(p*q))
+var p ="Hii"
+document.write('<br> p is ',p)
+// 3)Const keyword - Not possible redeclare & reassign
+const pi=3.14
+var r = 2.5
+document.write('<br> Area of circle is ',pi*r*r)
+//4)Let keyword --- cant redeclare but reassign is possible
+let str = "Test"
+document.write('<br> My name is '+str)
+
+ str = 100
+ document.write('<br>No is '+str)
+
+
+
+
