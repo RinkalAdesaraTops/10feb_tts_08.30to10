f4633c36cf6db01e1a1e86276affa67fdd6b654e
Rinkal <rinkaladesara.tops@gmail.com>
2025-05-08 12:29:45 +0530

    react router

--- reactjs/apiexample/src/App.jsx
+++ reactjs/apiexample/src/App.jsx
@@ -4,10 +4,14 @@ import ApiCrud from './ApiCrud'
 import CustomHook from './CustomHook'
 import Customhookrevision from './Customhookrevision'
 import { lazy, Suspense } from 'react'
+import Menu from './Menu'
+import { BrowserRouter, Routes,Route } from 'react-router'
 const Home = lazy(()=>import('./Home'))
 const Contact = lazy(()=>import('./Contact'))
+
 // import Home from './Home'
-// import Contact from './Contact'
+import Contact1 from './Contact1'
+import Contact2 from './Contact2'
 function App() {
   return (
     <>
@@ -15,13 +19,24 @@ function App() {
       {/* <ApiCrud/> */}
       {/* <CustomHook /> */}
      {/* <Customhookrevision/> */}
-     <Suspense fallback={<div>PLzz wait Contact loading...</div>}>
+     {/* <Suspense fallback={<div>PLzz wait Contact loading...</div>}>
         <Contact />
      </Suspense>
      <Suspense fallback={<h3>Home component coming...</h3>}>
         <Home />
-     </Suspense>
-     
+     </Suspense>    */}
+     <Menu />
+     {/* <BrowserRouter> */}
+     <Routes>
+        <Route path="/home" element={<Home />}/>
+        <Route path="/contact" element={<Contact />} >
+          <Route path='c1' element={<Contact1 />} />
+          <Route path='c2' element={<Contact2 />} />
+        </Route>
+        {/* <Route path="/contact" element={<Contact />}/> */}
+        <Route path="/crud" element={<ApiCrud />}/>
+     </Routes>
+     {/* </BrowserRouter> */}
     </>
   )
 }
--- reactjs/apiexample/src/Contact.jsx
+++ reactjs/apiexample/src/Contact.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { Outlet } from 'react-router'
 
 const Contact = () => {
     let [count,setCount] = useState("")
     useEffect(()=>{
-        for(let i=0;i<100000;i++){
+        for(let i=0;i<5;i++){
             count+=i
         }
         setCount(count)
@@ -11,6 +12,7 @@ const Contact = () => {
   return (
     <div>
       <h3>Count is --- {count}</h3>
+      <Outlet />
     </div>
   )
 }
